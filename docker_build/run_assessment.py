a6f3fd8e77cf154ebfb30b3505724fb5bf8e1e7f
Tyler Reddy <tyler.je.reddy@gmail.com>
2016-04-20 14:41:52 +0100

    Initial adjusments to run_assessment.py, in an effort to make the code easily calleable on the command line.

--- docker_build/run_assessment.py
+++ docker_build/run_assessment.py
@@ -1,36 +1,44 @@
 '''Author: Tyler Reddy
-   Purpose: To perform a multicore steric assessment using a standard test file along with the multiprocessing-based general-purpose module built above.'''
+   Purpose: To perform a multicore steric assessment using the multiprocessing-based general-purpose module.'''
 
-#import some useful modules:
-import MDAnalysis, time, numpy, scipy, sys
+import MDAnalysis
+import numpy as np
 import cPickle as pickle
-import steric_assessment_general #you may need to ensure that the module is in your path with sys.path.append('path/to/module') for example
+import steric_assessment_general 
+import argparse
+import matplotlib
+import matplotlib.pyplot
 
-start_time = time.time() #start time in seconds for benchmarking purposes
+parser = argparse.ArgumentParser()
+parser.add_argument("start_index", type=int)
+parser.add_argument("end_index", type=int)
+parser.add_argument("coord_filepath", type=str)
+parser.add_argument("particles_per_residue", type=int)
+parser.add_argument("cutoff", type=float, help="cutoff (A)")
+parser.add_argument("pickle_filename", type=str, default='steric_viols.p')
+parser.add_argument("plot_filename", type=str, default='steric_histogram.png')
+args = parser.parse_args()
 
-#produce an overall list of steric violations using the general multicore code in another module and convert to numpy array:
-array_DPPC_steric_violations = numpy.array(steric_assessment_general.main(start_index = 1,end_index = 10524,coordinate_file = 'dppc_vesicle.gro',particles_per_residue = 12, cutoff = 8.0)) #note that I've exaggerated the cutoff size a bit as the standard file doesn't likely have any major steric violations at, say, 2.0 A
+#produce an overall list of per-residue steric violations using the general multicore code in another module and convert to numpy array:
+array_steric_violations = np.array(steric_assessment_general.main(start_index = args.start_index,
+                                                                       end_index = args.end_index,
+                                                                       coordinate_file = args.coord_filepath,
+                                                                       particles_per_residue = args.particles_per_residue,
+                                                                       cutoff = ars.cutoff)) 
 
-#because this may take quite a while, pickle the array to be safe (see pickle docs for details):
-pickle.dump(array_DPPC_steric_violations,open('DPPC_steric_viols.p','wb'))
+#because this may take quite a while, pickle the array to be safe:
+pickle.dump(array_steric_violations,open(args.pickle_filename,'wb'))
 
-#when simply adjusting the plot, can load directly from pickle with the above code commented (no need to re-run analysis code unless you want to change parameters):
-#array_DPPC_steric_violations = pickle.load(open('DPPC_steric_viols.p','rb'))
-
-#now plot the data (see matplotlib docs):
-import matplotlib, matplotlib.pyplot
-
-fig=matplotlib.pyplot.figure()
+#now plot the data (default plot -- the user can use the pickled data to customize their own):
+fig = matplotlib.pyplot.figure()
 ax = fig.add_subplot(111)
 matplotlib.pyplot.xticks(rotation=90)
-histogram,bins = numpy.histogram(array_DPPC_steric_violations,bins=20)
-bincenters = 0.5*(bins[1:]+bins[:-1]) #adjust to center them 
-percent_denominator = array_DPPC_steric_violations.size / 100.0
-ax.bar(bincenters,histogram / percent_denominator,facecolor = 'green',alpha=0.75,width=2) #percent histogram
+histogram, bins = np.histogram(array_steric_violations,bins=20)
+bincenters = 0.5 * (bins[1:] + bins[:-1]) #adjust to center them 
+percent_denominator = array_steric_violations.size / 100.0
+ax.bar(bincenters, histogram / percent_denominator,facecolor = 'green',alpha=0.75,width=2) #percent histogram
 ax.set_xlim(-1,95)
-ax.set_xlabel('# of contacts within 8.0 $\AA$')
-ax.set_ylabel('% of DPPC residues')
+ax.set_xlabel('# of contacts within {cutoff} $\AA$'.format(cutoff=args.cutoff))
+ax.set_ylabel('% of residues')
 fig.set_size_inches(4,6)
-fig.savefig('DPPC_steric_histogram.png',dpi=300)
-
-print 'Steric assessment code completed in',time.time() - start_time, 'seconds'
+fig.savefig(args.plot_filename,dpi=300)
